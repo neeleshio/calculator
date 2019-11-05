80cf73ae6919ab70aba6a5bdf4ec084fbbbd982e
Neelesh Shetty <an.neelesh@novasemita.org>
2019-11-05 17:44:05 +0530

    functional calc

---
+++ script.js
@@ -0,0 +1,21 @@
+function disp(num) {
+    document.getElementById("input").value += num;
+}
+
+function allClr() {
+    document.getElementById("input").value = "";
+    document.getElementById("output").value = "";
+
+}
+
+function clr() {
+    var clear = document.getElementById("input").value;
+    var c = clear.substr(0, (clear.length - 1));
+    document.getElementById("input").value = c;
+}
+
+function solve() {
+    let x = document.getElementById("input").value;
+    let y = eval(x);
+    document.getElementById("output").value = y;
+}
